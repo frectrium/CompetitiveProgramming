c62e1ee13d3209ae1cc5f0fe1e930b656a70049f
Parshv Joshi <132247221+frectrium@users.noreply.github.com>
2023-11-14 18:02:59 +0530

    Update

--- template.cpp
+++ template.cpp
@@ -1,8 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vpii = vector<pair<int, int>>;
+using pii = pair<int, int>;
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define REP(i, a, b) for(int i = a; i < b; i++)
+#define PI 4*atan(1)
+/*------------------------------------------------------------*/
+void solve(){
+    
+    return;
+}
+
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    ll T = 1;
+    while(T--){
+        solve();
+    }
     return 0;
 }
